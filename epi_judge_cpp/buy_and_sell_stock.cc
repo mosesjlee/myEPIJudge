952bbaccb286ae3daf00bef8d5e5fd68ae833a74
moses_lee <moses_lee@apple.com>
2018-08-17 10:50:46 -0700

    Solved buy_and_sell_stock with help from book explanation

--- epi_judge_cpp/buy_and_sell_stock.cc
+++ epi_judge_cpp/buy_and_sell_stock.cc
@@ -3,7 +3,14 @@
 using std::vector;
 double BuyAndSellStockOnce(const vector<double>& prices) {
   // TODO - you fill in here.
-  return 0.0;
+	double curr_min = prices.front();
+	double curr_profit = 0.0;
+	for(int i = 1; i < prices.size(); i++) {
+		curr_min = fmin(curr_min, prices[i]);
+		curr_profit = fmax(prices[i] - curr_min, curr_profit);
+	}
+
+  return curr_profit;
 }
 
 int main(int argc, char* argv[]) {

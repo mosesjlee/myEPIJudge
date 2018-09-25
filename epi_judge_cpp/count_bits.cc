99771f1893004075ebdde703fdd334d7a9af5bbf
moses_lee <moses_lee@apple.com>
2018-09-25 09:44:13 -0700

    Got rid of java and python. Reviewed parity, counting bits, and swap bits

--- epi_judge_cpp/count_bits.cc
+++ epi_judge_cpp/count_bits.cc
@@ -1,8 +1,12 @@
 #include "test_framework/generic_test.h"
 
 short CountBits(unsigned int x) {
-  // TODO - you fill in here.
-  return 0;
+  short result = 0;
+  while(x) {
+    result += 1;
+    x = x & (x-1);
+  }
+  return result;
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,7 +1,31 @@
 #include "test_framework/generic_test.h"
+// #define OPTION_2
 short Parity(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+  short result = 0;
+#ifdef OPTION_1
+  //Optimized to only get rid of lowest set bit
+  while(x) {
+    result ^= 1;
+    x = x&(x-1);
+  }
+#elif defined OPTION_2
+  //The folding option
+  x ^= x >> 32;     
+  x ^= x >> 16;
+  x ^= x >> 8;
+  x ^= x >> 4;
+  x ^= x >> 2;
+  x ^= x >> 1;
+  result = x & 1;
+#else
+  //BRUTE FORCE
+  while(x) {
+    result ^= x & 1;
+    x >>= 1;
+  }
+#endif
+
+  return result;
 }
 
 int main(int argc, char* argv[]) {

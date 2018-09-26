6a7d8dc045bdcb6b49acc249fc84fc84374ac560
moses_lee <moses_lee@apple.com>
2018-09-26 09:43:17 -0700

    Figured out primitive multiply

--- epi_judge_cpp/primitive_multiply.cc
+++ epi_judge_cpp/primitive_multiply.cc
@@ -1,7 +1,46 @@
 #include "test_framework/generic_test.h"
+
+unsigned long long add(unsigned long long x, unsigned long long y) {
+  unsigned long long result = 0;
+  int shift_count = 0;
+
+  //THESE USED TO BE INT BUT I RAN INTO INTEGER OVERFLOW USE LONG IF SPECIFIED LONG
+  long long curr_x_bit = 0, curr_y_bit = 0;
+  long long added_bit = 0, carry_over = 0;
+  while(x != 0 || y != 0) {
+    curr_x_bit = x & 1;
+    curr_y_bit = y & 1;
+    added_bit = curr_x_bit ^ curr_y_bit ^ carry_over;
+    carry_over = (curr_x_bit & curr_y_bit) | (curr_x_bit & carry_over) | (curr_y_bit & carry_over);
+    result |= (added_bit << shift_count);
+    x >>= 1; y >>= 1;
+    shift_count++;
+  }
+
+  return result |= (carry_over << shift_count);
+}
+
 unsigned long long Multiply(unsigned long long x, unsigned long long y) {
   // TODO - you fill in here.
-  return 0;
+// #define BRUTE_FORCE
+#ifdef BRUTE_FORCE
+  if(x == 0 || y == 0) return 0;
+  unsigned long long result = 0;
+  unsigned int start = 0;
+  while(start < y) {
+    result += add(x, 0);
+    start++;
+  }
+#else
+  long long result = 0;
+  while(x) {
+    if(x & 1) {
+      result = add(result, y);
+    }
+    x >>= 1; y <<= 1;
+  }
+#endif
+  return result;
 }
 
 int main(int argc, char* argv[]) {

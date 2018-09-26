d81e771be97bd988451bad8ea6de941491a4de4e
moses_lee <moses_lee@apple.com>
2018-09-26 10:12:03 -0700

    Previous add cheated by using ++ operator

--- epi_judge_cpp/primitive_multiply.cc
+++ epi_judge_cpp/primitive_multiply.cc
@@ -1,6 +1,7 @@
 #include "test_framework/generic_test.h"
 
-unsigned long long add(unsigned long long x, unsigned long long y) {
+//THIS IS CHEATING!!! i used the "++" operator in this implementation
+unsigned long long addCheating(unsigned long long x, unsigned long long y) {
   unsigned long long result = 0;
   int shift_count = 0;
 
@@ -14,10 +15,30 @@ unsigned long long add(unsigned long long x, unsigned long long y) {
     carry_over = (curr_x_bit & curr_y_bit) | (curr_x_bit & carry_over) | (curr_y_bit & carry_over);
     result |= (added_bit << shift_count);
     x >>= 1; y >>= 1;
-    shift_count++;
+    shift_count++; //CHEATING!!!
   }
 
   return result |= (carry_over << shift_count);
+}
+
+unsigned long long add(unsigned long long x, unsigned long long y) {
+  unsigned long long result = 0;
+  long long shifter = 1;
+  long long curr_x_bit = 0, curr_y_bit = 0, temp_x = x, temp_y = y;
+  long long carry_over = 0, carry_in = 0;
+
+  while(temp_x || temp_y) {
+    curr_x_bit = x & shifter;
+    curr_y_bit = y & shifter;
+    carry_over = (curr_x_bit & curr_y_bit) | (curr_x_bit & carry_in) | (curr_y_bit & carry_in);
+    result |= curr_x_bit ^ curr_y_bit ^ carry_in;
+    carry_in = carry_over << 1; 
+    temp_y >>= 1;
+    temp_x >>= 1;
+    shifter <<= 1; 
+  }
+
+  return result |= carry_in;
 }
 
 unsigned long long Multiply(unsigned long long x, unsigned long long y) {

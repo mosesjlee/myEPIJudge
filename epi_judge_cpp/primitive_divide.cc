246be53aebce6c04270967d1a23a28153c781d36
moses_lee <moses_lee@apple.com>
2018-09-27 10:10:37 -0700

    implemented divide with a lot of help from the book

--- epi_judge_cpp/primitive_divide.cc
+++ epi_judge_cpp/primitive_divide.cc
@@ -1,8 +1,43 @@
 #include "test_framework/generic_test.h"
 int Divide(int x, int y) {
   // TODO - you fill in here.
-  return 0;
+  //my version.
+  //CAN'T USE * or /
+  //I had to break it down because the books way used bit manipulation
+  //That is not very obvious. My comments show where i correlated the 
+  //Concepts in the book
+  //Have to commit memory that 2^k is equivallent to 1 << k
+  //Have to commit memory that 2^k * k is equivallent to k << k
+  
+  long power = 32; //Limit to 32 to prevent overflow
+  unsigned long y_power = (long) y << power; //To simulate 2 ^ k * y where k == y 
+  long quotient = 0;
+  long x_cast = (long) x;
+  long y_cast = (long) y; 
+  while (x_cast >= y_cast) {
+    if(y_power <= x_cast) {
+      x_cast -= y_power;
+      quotient += 1 << power; //To simulate quotient += 2 ^ k
+    }
+    else {
+      y_power >>= 1;
+      power--;
+    }
+  }
+  return quotient;
 }
+
+  // int quotient = 0;
+  // while (x >= y) {
+  //   while(y_power > x) {
+  //     y_power >>= 1;
+  //     power--;
+  //   }
+
+  //   x -= y_power;
+  //   quotient += 1 << power;
+  // }
+  // return quotient;
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};

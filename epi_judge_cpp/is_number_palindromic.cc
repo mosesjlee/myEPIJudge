9267f36f02166a1f7edcf2d569ef2666b07fe2cf
moses_lee <moses_lee@apple.com>
2018-09-27 16:23:54 -0700

    solved is_number_palindromic

--- epi_judge_cpp/is_number_palindromic.cc
+++ epi_judge_cpp/is_number_palindromic.cc
@@ -1,6 +1,23 @@
 #include "test_framework/generic_test.h"
 bool IsPalindromeNumber(int x) {
-  // TODO - you fill in here.
+  if(x <= 0) return x == 0;
+
+  long places = 0;
+  double floored = floor(log10(x)); //Use log10 to get number of 10's places
+  places = pow(10, floored);
+  places = places == 0 ? 1 : places;
+
+  while(places > 1) {
+    long lsd = x % 10;
+    long msd = x / places;
+    if(lsd != msd)
+      return false;
+
+    x /= 10;
+    x %= (places/10);
+    places /= 100;
+  }
+
   return true;
 }
 

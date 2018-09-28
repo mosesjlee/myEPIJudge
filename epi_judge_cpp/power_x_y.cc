a16d162e557932001416f9f7fe12895b83959f66
moses_lee <moses_lee@apple.com>
2018-09-28 09:56:19 -0700

    Figured out rectangle_intersection, understand how power_x_y works

--- epi_judge_cpp/power_x_y.cc
+++ epi_judge_cpp/power_x_y.cc
@@ -1,7 +1,22 @@
 #include "test_framework/generic_test.h"
 double Power(double x, int y) {
   // TODO - you fill in here.
-  return 0.0;
+  double result = 1.0;
+  long power = y;
+  if(y < 0) {
+    power = -power;
+    x = 1.0/x;
+  }
+
+  while(power) {
+    if(power & 1) {
+      result *= x;
+    }
+
+    x *= x;
+    power >>= 1;
+  }
+  return result;
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/rectangle_intersection.cc
+++ epi_judge_cpp/rectangle_intersection.cc
@@ -6,14 +6,36 @@ struct Rect {
   int x, y, width, height;
 };
 
-Rect IntersectRectangle(const Rect& R1, const Rect& R2) {
-  // TODO - you fill in here.
-  return {0, 0, 0, 0};
-}
 bool operator==(const Rect& r1, const Rect& r2) {
   return std::tie(r1.x, r1.y, r1.width, r1.height) ==
          std::tie(r2.x, r2.y, r2.width, r2.height);
 }
+
+Rect IntersectRectangle(const Rect& R1, const Rect& R2) {
+  // TODO - you fill in here.
+  if(R1 == R2) return R1;
+
+  Rect retval = {0, 0, -1, -1};
+
+  //My idea is take the difference of the starting point
+  //If the difference is greater than the width the side is not
+  //intersecting
+  if(R1.x - R2.x > R2.width || R2.x - R1.x > R1.width) {
+    return retval;
+  }
+  if(R1.y - R2.y > R2.height || R2.y - R1.y > R1.height) {
+    return retval;
+  }
+
+  //Find the lowest point out and subtract the greatest location point
+  retval.width = fmin(R1.x + R1.width, R2.x + R2.width) - fmax(R1.x, R2.x);
+  retval.height = fmin(R1.y + R1.height, R2.y+ R2.height) - fmax(R1.y, R2.y);
+
+  retval.x = fmax(R1.x, R2.x);
+  retval.y = fmax(R1.y, R2.y);
+  return retval;
+}
+
 
 template <>
 struct SerializationTraits<Rect> : UserSerTraits<Rect, int, int, int, int> {

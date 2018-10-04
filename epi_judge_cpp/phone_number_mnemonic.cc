711b1b2a0ded682dd34a5703e410dfcbfb1867a0
moses_lee <moses_lee@apple.com>
2018-10-04 08:38:25 -0700

    figured out reverse_words and phone_number_mneomnic

--- epi_judge_cpp/phone_number_mnemonic.cc
+++ epi_judge_cpp/phone_number_mnemonic.cc
@@ -3,9 +3,50 @@
 #include "test_framework/generic_test.h"
 using std::string;
 using std::vector;
+
+#define FROM_THE_BACK
+// #define FROM_THE_FRONT
+
+vector<vector<string>> phone_pad = {{"0"}, 
+                                   {"1"}, {"A","B","C"}, {"D", "E", "F"}, 
+                                   {"G", "H", "I"}, {"J","K","L"}, {"M", "N", "O"}, 
+                                   {"P", "Q", "R", "S"}, {"T","U","V"}, {"W", "X", "Y", "Z"}};
 vector<string> PhoneMnemonic(const string& phone_number) {
   // TODO - you fill in here.
-  return {};
+#ifdef FROM_THE_BACK
+  if(phone_number == "") return {};
+  
+  vector<string> words_so_far = PhoneMnemonic(phone_number.substr(1, phone_number.length()));
+
+  int index = phone_number[0] - '0';
+  std::cout << "my number " << phone_number[0] << std::endl;
+  if(words_so_far.size() == 0) return phone_pad[index];
+
+  vector<string> retval;
+    for(string s : words_so_far) {
+      for(string my_c : phone_pad[index]) {
+      retval.push_back(std::string(my_c+s));
+    }
+  }
+  return retval;
+#elif defined FROM_THE_FRONT
+
+  if(phone_number == "") return {};
+  
+  vector<string> words_so_far = PhoneMnemonic(phone_number.substr(0, phone_number.length()-1));
+
+  int index = phone_number[phone_number.length()-1] - '0';
+  std::cout << "my number " << phone_number[phone_number.length()-1] << std::endl;
+  if(words_so_far.size() == 0) return phone_pad[index];
+
+  vector<string> retval;
+    for(string s : words_so_far) {
+      for(string my_c : phone_pad[index]) {
+      retval.push_back(std::string(s+my_c));
+    }
+  }
+  return retval;
+#endif
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/reverse_words.cc
+++ epi_judge_cpp/reverse_words.cc
@@ -5,6 +5,19 @@ using std::string;
 
 void ReverseWords(string* s) {
   // TODO - you fill in here.
+  if(s->length() == 1 || s->length() == 0) return;
+  string & reverse_copy = *s;
+  
+  //Reverse the whole string
+  reverse(reverse_copy.begin(), reverse_copy.end());
+  
+  int first_space = 0;
+  int start = 0;
+  while((first_space = reverse_copy.find_first_of(' ', start)) != string::npos) {
+    reverse(reverse_copy.begin()+start, reverse_copy.begin()+first_space);
+    start = first_space+1;
+  }
+  reverse(reverse_copy.begin()+start, reverse_copy.end());
   return;
 }
 string ReverseWordsWrapper(TimedExecutor& executor, string s) {

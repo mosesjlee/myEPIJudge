50df3144e443704fe926e6f81e47a8bc930da80b
moses_lee <moses_lee@apple.com>
2018-10-07 08:47:41 -0700

    Figured out evaluate_rpn. but the book has some new syntax i haven't seen

--- epi_judge_cpp/evaluate_rpn.cc
+++ epi_judge_cpp/evaluate_rpn.cc
@@ -1,9 +1,52 @@
 #include <string>
 #include "test_framework/generic_test.h"
+#include <stack>
 using std::string;
+
+int evaluated_expr(string & token, std::stack<int> * ref) {
+  std::stack<int> & copy = *ref;
+  if(token == "-") {
+    int val_2 = copy.top(); copy.pop();
+    int val_1 = copy.top(); copy.pop();
+    return val_1-val_2;
+  }
+  else if(token == "+") {
+    int val_2 = copy.top(); copy.pop();
+    int val_1 = copy.top(); copy.pop();
+    return val_1+val_2;
+  }
+  else if(token == "*") {
+    int val_2 = copy.top(); copy.pop();
+    int val_1 = copy.top(); copy.pop();
+    return val_1 * val_2;
+  }
+  else if(token == "/") {
+    int val_2 = copy.top(); copy.pop();
+    int val_1 = copy.top(); copy.pop();
+    return val_1/val_2;
+  }
+  else {
+    return atoi(token.c_str());
+  }
+}
+
 int Evaluate(const string& expression) {
   // TODO - you fill in here.
-  return 0;
+  std::stack<int> rpn;
+  size_t start = 0;
+  size_t comma_idx = 0;
+  string token = "";
+  while((comma_idx = expression.find(',', start)) != string::npos) {
+    comma_idx = expression.find(',', start);
+    token = expression.substr(start, comma_idx-start);
+    rpn.push(evaluated_expr(token, &rpn));
+    start = comma_idx + 1;
+  }
+
+  //Take care of the last expression
+  token = expression.substr(start, expression.length()-start); 
+  rpn.push(evaluated_expr(token, &rpn));
+  return rpn.top();
 }
 
 int main(int argc, char* argv[]) {

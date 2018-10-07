f35b0a534c3b1e8a1e007eb972435309662cd186
moses_lee <moses_lee@apple.com>
2018-10-06 19:44:47 -0700

    The book example image on even_odd_list_merge doesnt line up with their code

--- epi_judge_cpp/even_odd_list_merge.cc
+++ epi_judge_cpp/even_odd_list_merge.cc
@@ -2,7 +2,28 @@
 #include "test_framework/generic_test.h"
 shared_ptr<ListNode<int>> EvenOddMerge(const shared_ptr<ListNode<int>>& L) {
   // TODO - you fill in here.
-  return nullptr;
+  shared_ptr<ListNode<int>> even_dummy_head = make_shared<ListNode<int>>(ListNode<int>{0, L});
+  shared_ptr<ListNode<int>> odd_dummy_head = make_shared<ListNode<int>>(ListNode<int>{0, nullptr});
+  auto even_list_head = even_dummy_head;
+  auto even_iter = even_dummy_head->next;
+  auto odd_list_head = odd_dummy_head;
+
+  while(even_iter) {
+    if(even_iter->data & 1) {
+      even_list_head->next = even_iter->next;
+      odd_list_head->next = even_iter;
+      even_iter->next = nullptr;
+      odd_list_head = odd_list_head->next;
+      even_iter = even_list_head->next;
+    }
+    else {
+      even_list_head = even_list_head->next;
+      even_iter = even_iter->next;
+    }
+  }
+
+  even_list_head->next = odd_dummy_head->next;
+  return even_dummy_head->next;
 }
 
 int main(int argc, char* argv[]) {

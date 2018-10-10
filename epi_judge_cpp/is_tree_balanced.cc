bda3771a8c9ab74a35abdbb739520741880881b7
moses_lee <moses_lee@apple.com>
2018-10-10 07:13:38 -0700

    Figured out is_tree_balanced and is_tree_symmetric brute force

--- epi_judge_cpp/is_tree_balanced.cc
+++ epi_judge_cpp/is_tree_balanced.cc
@@ -1,8 +1,41 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
+#include <stack>
+int tree_height(BinaryTreeNode<int> * root);
+bool is_tree_balanced(BinaryTreeNode<int> * root, int & height);
+
 bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
   // TODO - you fill in here.
-  return true;
+  BinaryTreeNode<int> * root = tree.get();
+  int height = 0;
+  return is_tree_balanced(root, height);
+}
+
+bool is_tree_balanced(BinaryTreeNode<int> * root, int & height) {
+  if(root == nullptr) {
+    height = -1;
+    return true;
+  }
+
+  int left_height = 0, right_height = 0;
+  bool left_balanced = is_tree_balanced(root->left.get(), left_height);
+  left_height += 1;
+  bool right_balanced = is_tree_balanced(root->right.get(), right_height);
+  right_height += 1;
+
+  height = fmax(left_height, right_height);
+  int heigh_diff = abs(left_height-right_height);
+
+  return left_balanced && right_balanced && (heigh_diff <= 1);
+}
+
+int tree_height(BinaryTreeNode<int> * root) {
+  if(root == nullptr) return -1;
+
+  int left_height = tree_height(root->left.get());
+  int right_height = tree_height(root->right.get());
+
+  return left_height > right_height ? left_height + 1 : right_height + 1;
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/is_tree_symmetric.cc
+++ epi_judge_cpp/is_tree_symmetric.cc
@@ -1,9 +1,66 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
-
+bool IsSymmetricHelper(BinaryTreeNode<int> * root_1, BinaryTreeNode<int> * root_2);
+bool IsTreeEqual(BinaryTreeNode<int> * root_1, BinaryTreeNode<int> * root_2);
+void FlipTree(BinaryTreeNode<int> * root);
+// #define BRUTE_FORCE
 bool IsSymmetric(const unique_ptr<BinaryTreeNode<int>>& tree) {
   // TODO - you fill in here.
-  return true;
+  if(tree == nullptr) {
+    return true;
+  }
+
+  BinaryTreeNode<int> * left = tree->left.get();
+  BinaryTreeNode<int> * right = tree->right.get();
+#ifdef BRUTE_FORCE
+#pragma message ("BRUTE_FORCE")
+  FlipTree(left);
+
+  return IsTreeEqual(left, right);
+#else
+#pragma message ("OPTIMIZED")
+  return IsSymmetricHelper(left, right);
+#endif
+}
+
+bool IsSymmetricHelper(BinaryTreeNode<int> * root_1, BinaryTreeNode<int> * root_2){
+  if(root_1 == nullptr && root_2 == nullptr) {
+    return true;
+  }
+
+  //Since the first if checks for 2 of them being nullptr
+  //We can just check if one of them is nullptr
+  if(root_1 == nullptr || root_2 == nullptr) {
+    return false;
+  }
+
+  bool left_equal = IsSymmetricHelper(root_1->left.get(), root_2->right.get());
+  bool right_equal = IsSymmetricHelper(root_1->right.get(), root_2->left.get());
+
+  return left_equal && right_equal && (root_1->data == root_2->data);
+}
+
+bool IsTreeEqual(BinaryTreeNode<int> * root_1, BinaryTreeNode<int> * root_2) {
+  if(root_1 == nullptr && root_2 == nullptr) {
+    return true;
+  }
+  if((root_1 == nullptr && root_2 != nullptr) || (root_1 != nullptr && root_2 == nullptr)) {
+    return false;
+  }
+
+  bool left_equal = IsTreeEqual(root_1->left.get(), root_2->left.get());
+  bool right_equal = IsTreeEqual(root_1->right.get(), root_2->right.get());
+
+  return left_equal && right_equal && (root_1->data == root_2->data);
+}
+
+void FlipTree(BinaryTreeNode<int> * root) {
+  if(root == nullptr) return;
+
+  FlipTree(root->left.get());
+  FlipTree(root->right.get());
+
+  root->left.swap(root->right);
 }
 
 int main(int argc, char* argv[]) {

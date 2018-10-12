f84c0ecb73999719ac85f7924273df34a1d816ef
moses_lee <moses_lee@apple.com>
2018-10-11 22:10:30 -0700

    had to get extra help to solve tree_with_parent_inorder and tree_from_preorder_inorder

--- epi_judge_cpp/tree_from_preorder_inorder.cc
+++ epi_judge_cpp/tree_from_preorder_inorder.cc
@@ -4,10 +4,90 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+#define BRUTE_FORCE
+
+BinaryTreeNode<int> * BinaryTreeFromPreorderInorderHelper(const vector<int>& preorder, const vector<int>& inorder, int & preorder_index);
+BinaryTreeNode<int> * BTFromPreorderInorderHelperWithoutExtraVectors(const vector<int>& preorder, const vector<int>& inorder, int & preorder_index);
+
 unique_ptr<BinaryTreeNode<int>> BinaryTreeFromPreorderInorder(
     const vector<int>& preorder, const vector<int>& inorder) {
   // TODO - you fill in here.
-  return nullptr;
+#ifdef BRUTE_FORCE
+  int preorder_index = 0;
+  return unique_ptr<BinaryTreeNode<int>>(BinaryTreeFromPreorderInorderHelper(preorder, inorder, preorder_index));
+#else
+  int preorder_index = 0;
+  return unique_ptr<BinaryTreeNode<int>>(BinaryTreeFromPreorderInorderHelper2(preorder, inorder, preorder_index));
+ #endif
+}
+
+void print_vector(const vector<int>& v) {
+  for(int i : v) {
+    std::cout << i << " ,";
+  }
+  std::cout << std::endl;
+}
+
+
+
+int get_index_of_element(const vector<int> & v, int elem) {
+  int retval = -1;
+  for(int i = 0; i < v.size(); i++) {
+    if(elem == v[i]) {
+      retval = i;
+      break;
+    }
+  }
+  return retval;
+}
+
+BinaryTreeNode<int> * BTFromPreorderInorderHelperWithoutExtraVectors(const vector<int>& preorder, 
+                                                                     const vector<int>& inorder, 
+                                                                     int & preorder_index, int inorder_start, int inorder_end)
+{
+  
+  BinaryTreeNode<int> * root = new BinaryTreeNode<int>(preorder[0]);
+  int root_val = root->data;
+  int root_index = get_index_of_element(inorder, root_val);
+
+  auto left = BTFromPreorderInorderHelperWithoutExtraVectors(preorder);
+  root->left.reset(left);
+
+  auto right = BTFromPreorderInorderHelperWithoutExtraVectors(preorder);
+  root->right.reset(right);
+
+  return root;
+}
+
+vector<int> get_subvector(const vector<int> & v, int start, int end) {
+  if(start >= end) return {};
+  return vector<int>(v.begin()+start, v.begin()+end);
+}
+
+BinaryTreeNode<int> * BinaryTreeFromPreorderInorderHelper(
+  const vector<int>& preorder, const vector<int>& inorder, int & preorder_index) {
+  if(inorder.size() == 0) {
+    return nullptr;
+  }
+
+  BinaryTreeNode<int> * root = new BinaryTreeNode<int>(preorder[preorder_index]);
+  preorder_index++;
+  if(inorder.size() == 1) {
+    return root;
+  }
+
+  int root_val = root->data;
+  int root_index = get_index_of_element(inorder, root_val);
+  vector<int> first_half = get_subvector(inorder, 0, root_index);
+  vector<int> second_half = get_subvector(inorder, root_index+1, inorder.size());
+
+  BinaryTreeNode<int> * left = BinaryTreeFromPreorderInorderHelper(preorder, first_half, preorder_index);
+  root->left.reset(left);
+
+  BinaryTreeNode<int> * right  = BinaryTreeFromPreorderInorderHelper(preorder, second_half, preorder_index);
+  root->right.reset(right);
+
+  return root;
 }
 
 int main(int argc, char* argv[]) {
--- epi_judge_cpp/tree_with_parent_inorder.cc
+++ epi_judge_cpp/tree_with_parent_inorder.cc
@@ -3,9 +3,86 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+#define BRUTE_FORCE
+
 vector<int> InorderTraversal(const unique_ptr<BinaryTreeNode<int>>& tree) {
   // TODO - you fill in here.
-  return {};
+  vector<int> retval;
+  BinaryTreeNode<int> * iter = tree.get();
+  BinaryTreeNode<int> * prev = nullptr;
+
+#ifdef BRUTE_FORCE
+#pragma message("Brute Force")
+  //Start at the left most node
+  while(iter && iter->left) {
+    iter = iter->left.get();
+  }
+
+  while(iter != nullptr) {
+    if(prev == nullptr || prev == iter->left.get()) {
+      retval.push_back(iter->data);
+    }
+    else if(prev == iter->right.get()) {
+      prev = iter;
+      iter = iter->parent;
+      continue;
+    }
+
+    if(iter->right) {
+      prev = nullptr;
+      iter = iter->right.get();
+      while(iter->left) {
+        iter = iter->left.get();
+      }
+    }
+    else {
+      prev = iter;
+      iter = iter->parent;
+    }
+  }
+#else //Optimized
+#pragma message("Optimized")
+
+  //This is the book's optmized version
+  //I expanded the ternary and the temporary variable 'next' to better understand
+
+  while(iter != nullptr) {
+    if(prev == iter->parent) {
+      if(iter->left != nullptr) {
+        prev = iter;
+        iter = iter->left.get();
+      }
+      else {
+        retval.push_back(iter->data);
+        if(iter->right != nullptr) {
+          prev = iter;
+          iter = iter->right.get();
+        }
+        else {
+          prev = iter;
+          iter = iter->parent;
+        }
+      }
+    }
+    else if(prev == iter->left.get()) {
+      retval.push_back(iter->data);
+      if(iter->right != nullptr) {
+        prev = iter;
+        iter = iter->right.get();
+      }
+      else {
+        prev = iter;
+        iter = iter->parent;
+      }
+    }
+    else {
+      prev = iter;
+      iter = iter->parent;
+    }
+  }
+
+#endif
+  return retval;
 }
 
 int main(int argc, char* argv[]) {

f3d74ca1c58f5c41b2a68818d6053c851d08bb9a
moses_lee <moses_lee@apple.com>
2018-10-19 17:53:32 -0700

    Figured out brute force calendar_rendering

--- epi_judge_cpp/calendar_rendering.cc
+++ epi_judge_cpp/calendar_rendering.cc
@@ -2,13 +2,35 @@
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 using std::vector;
-
+#define BRUTE_FORCE
 struct Event {
   int start, finish;
 };
 int FindMaxSimultaneousEvents(const vector<Event>& A) {
   // TODO - you fill in here.
-  return 0;
+  int layers = 0;
+#ifdef BRUTE_FORCE
+  vector<Event> A_copy(A);
+  //1st sort
+  std::sort(A_copy.begin(), A_copy.end(), [](const Event & a, const Event & b){ return a.start < b.start; });
+  
+  while(!A_copy.empty()) {
+    int prev_finish = -1;
+    auto it = A_copy.begin();
+    while(it != A_copy.end()) {
+      if(prev_finish < it->start) {
+        prev_finish = it->finish;
+        it = A_copy.erase(it);
+      }
+      else {
+        it++;
+      }
+    }
+    layers++;
+  }
+#else
+#endif
+  return layers;
 }
 template <>
 struct SerializationTraits<Event> : UserSerTraits<Event, int, int> {};
